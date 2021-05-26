439862478e0c35f5dd45bc18ab297d043167c8ff
Seop2 <lk9050@naver.com>
2021-05-26 14:43:15 +0900

    add custom website

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,13 +4,13 @@
       <ul>
         <li><nuxt-link to="/"><i class="fas fa-home"></i> 홈으로</nuxt-link></li>
       </ul>
+      </nav>
       <nuxt/>
       <footer class="footer">
         <div class="content has-text-centered">
           <i class="fas fa-copyright"></i> 2020-2021, 모든 저작권은 <strong>js</strong>에 있습니다.
         </div>
       </footer>
-    </nav>
   </div>
 </template>
 
