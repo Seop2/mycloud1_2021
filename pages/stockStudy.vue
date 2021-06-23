f00a37dcf80c80dca4d8d5c18e2d0ccb428a5cac
Seop2 <lk9050@naver.com>
2021-06-23 15:41:08 +0900

    add stockStudy.vue

---
+++ pages/stockStudy.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <section class="hero is-danger">
+            <div class="hero-body">
+                <p class="title">
+                    <i class="fas fa-graduation-cap">주식용어정리</i>
+                </p>
+                <p class="subtitle">
+                    차트분석에 필요한 용어를 정리해봅시다~
+                </p>
+            </div>
+        </section>
+        
+        <section class="columns"></section>
+        <div class="column"></div>
+        <div class="column"></div>
+        
+    </div>
+</template>
